616bb8220103e97ee72d59558deb2e1130e7b556
Ben Saufley <bensaufley+git@gmail.com>
2016-12-07 00:21:37 -0500

    Wordpress permalinks allow percent sign

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,10 @@ Rails.application.routes.draw do
     get '/type/:type(/:page)', to: 'blog_posts#index', as: :admin_posts_by_type
   end
 
-  get '/*permalink/edit', to: 'admin/blog_posts#edit', permalink: %r{\d{4}/\d{2}/[\w\-]+}, as: :edit_blog_post
-  get '/*permalink', to: 'blog_posts#show', permalink: %r{\d{4}/\d{2}/[\w\-]+}, as: :blog_post
-  match '/*permalink', to: 'admin/blog_posts#update', via: [:put, :patch], permalink: %r{\d{4}/\d{2}/[\w\-]+}
-  delete '/*permalink', to: 'admin/blog_posts#destroy', permalink: %r{\d{4}/\d{2}/[\w\-]+}
+  get '/*permalink/edit', to: 'admin/blog_posts#edit', permalink: %r{\d{4}/\d{2}/[\w\-%]+}, as: :edit_blog_post
+  get '/*permalink', to: 'blog_posts#show', permalink: %r{\d{4}/\d{2}/[\w\-%]+}, as: :blog_post
+  match '/*permalink', to: 'admin/blog_posts#update', via: [:put, :patch], permalink: %r{\d{4}/\d{2}/[\w\-%]+}
+  delete '/*permalink', to: 'admin/blog_posts#destroy', permalink: %r{\d{4}/\d{2}/[\w\-%]+}
 
   root to: 'blog_posts#index'
 end
